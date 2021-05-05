13693e00efcefeb885a989fddd0c34af9cbecc58
Reynaldo Quispe <reynaldo.quispe@buildout.com>
2021-05-05 01:52:24 -0400

    3: actualizacion del README.md para la instalacion de Flyway, creacion de tablas:  'Location', 'AirportOperator' y 'Airport'. Llenado de 10 registros en cada tabla

--- role/fullstack/senior/reynaldoqs/main-services/README.md
+++ role/fullstack/senior/reynaldoqs/main-services/README.md
@@ -12,12 +12,37 @@ Tener  **Docker** instalado. [guia de instalacion](https://docs.docker.com/engin
 
 ### Despliegue MySQL con docker
 
+**nota:** `<[data]>` el keyword `data` puede ser remplazado con los datos propios de una entorno de trabajo.
+
 Para hacer un despliegue simple de una instancia de 'MySQL' ejecutar el siguiente commando:
 
-`docker run --name mysql-test -p 3306:3306 -e MYSQL_ROOT_PASSWORD=[password] -d mysql`
+`docker run --name mysql-azulo -p 3306:3306 -e MYSQL_ROOT_PASSWORD=<yourPasswordHere> -e MYSQL_DATABASE=<azulo> -d mysql`
+
 
 Para verificar el funcionando ejecutar:
 
 `docker ps`
 
 Debe mostrar una lista de contenedores corriendo y entre ellas debe estar uno con el nombre **mysql-test**. [Mas informacion](https://hub.docker.com/_/mysql)
+
+### Configuracion Flyway
+
+Instalar **flyway** [guia instalacion](https://flywaydb.org/documentation/getstarted/firststeps/commandline), video de migraciones y configuracion con **flyway** [video](https://www.youtube.com/watch?v=JRhuoC_kI8Y).
+
+Configurado **flyway** ejecutar los siguientes pasos para terminar la migracion de la base de datos:
+
+- Configurar el archivo `[ruta de flyway]/conf/flyway.conf` con la siguiente informacion(remplazar campos):
+
+```
+flyway.url=<jdbc:mysql://127.0.0.1:3306/azulo>
+flyway.user=<root>
+flyway.password=<yourPasswordHere>
+```
+
+- Copiar el archivo `V1_0_1__Create_tables_records.sql` a `[ruta de flyway]/sql/`.
+
+- Ejecutar el siguiente comando para realizar la migracion:
+
+`flyway migration`
+
+
---
+++ role/fullstack/senior/reynaldoqs/main-services/V1_0_1__Create_tables_records.sql
@@ -0,0 +1,81 @@
+#
+# TABLE STRUCTURE FOR: AirportOperator
+#
+
+DROP TABLE IF EXISTS `AirportOperator`;
+
+CREATE TABLE `AirportOperator` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `AirportOperator` (`ID`, `Name`) VALUES (1, 'provident');
+INSERT INTO `AirportOperator` (`ID`, `Name`) VALUES (2, 'et');
+INSERT INTO `AirportOperator` (`ID`, `Name`) VALUES (3, 'qui');
+INSERT INTO `AirportOperator` (`ID`, `Name`) VALUES (4, 'eum');
+INSERT INTO `AirportOperator` (`ID`, `Name`) VALUES (5, 'quo');
+INSERT INTO `AirportOperator` (`ID`, `Name`) VALUES (6, 'qui');
+INSERT INTO `AirportOperator` (`ID`, `Name`) VALUES (7, 'asperiores');
+INSERT INTO `AirportOperator` (`ID`, `Name`) VALUES (8, 'a');
+INSERT INTO `AirportOperator` (`ID`, `Name`) VALUES (9, 'quaerat');
+INSERT INTO `AirportOperator` (`ID`, `Name`) VALUES (10, 'maiores');
+
+
+#
+# TABLE STRUCTURE FOR: Location
+#
+
+DROP TABLE IF EXISTS `Location`;
+
+CREATE TABLE `Location` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Country` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ProvinceOrState` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Longitude` decimal(9,6) DEFAULT NULL,
+  `Latitude` decimal(8,6) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `Location` (`ID`, `Country`, `ProvinceOrState`, `Longitude`, `Latitude`) VALUES (1, 'UT', 'Missouri', '8.000000', '7.000000');
+INSERT INTO `Location` (`ID`, `Country`, `ProvinceOrState`, `Longitude`, `Latitude`) VALUES (2, 'WA', 'Georgia', '7.000000', '4.000000');
+INSERT INTO `Location` (`ID`, `Country`, `ProvinceOrState`, `Longitude`, `Latitude`) VALUES (3, 'IL', 'Vermont', '9.000000', '9.000000');
+INSERT INTO `Location` (`ID`, `Country`, `ProvinceOrState`, `Longitude`, `Latitude`) VALUES (4, 'WA', 'Idaho', '8.000000', '8.000000');
+INSERT INTO `Location` (`ID`, `Country`, `ProvinceOrState`, `Longitude`, `Latitude`) VALUES (5, 'LA', 'Kansas', '9.000000', '7.000000');
+INSERT INTO `Location` (`ID`, `Country`, `ProvinceOrState`, `Longitude`, `Latitude`) VALUES (6, 'SD', 'District of Columbia', '3.000000', '3.000000');
+INSERT INTO `Location` (`ID`, `Country`, `ProvinceOrState`, `Longitude`, `Latitude`) VALUES (7, 'LA', 'NorthCarolina', '5.000000', '9.000000');
+INSERT INTO `Location` (`ID`, `Country`, `ProvinceOrState`, `Longitude`, `Latitude`) VALUES (8, 'AK', 'Nevada', '4.000000', '7.000000');
+INSERT INTO `Location` (`ID`, `Country`, `ProvinceOrState`, `Longitude`, `Latitude`) VALUES (9, 'AR', 'NorthDakota', '2.000000', '6.000000');
+INSERT INTO `Location` (`ID`, `Country`, `ProvinceOrState`, `Longitude`, `Latitude`) VALUES (10, 'CT', 'Arkansas', '0.000000', '3.000000');
+
+
+#
+# TABLE STRUCTURE FOR: AirPort
+#
+
+DROP TABLE IF EXISTS `AirPort`;
+
+CREATE TABLE `AirPort` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `AirPortCode` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `PriorityOrder` int(11) DEFAULT NULL,
+  `LocationID` int(11) NOT NULL,
+  `AirportOperatorID` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `LocationID` (`LocationID`),
+  UNIQUE KEY `AirportOperatorID` (`AirportOperatorID`),
+  CONSTRAINT `FK_AirPortLocation` FOREIGN KEY (`LocationID`) REFERENCES `Location` (`ID`),
+  CONSTRAINT `FK_FK_AirPortOperator` FOREIGN KEY (`AirportOperatorID`) REFERENCES `AirportOperator` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `AirPort` (`ID`, `Name`, `AirPortCode`, `PriorityOrder`, `LocationID`, `AirportOperatorID`) VALUES (1, 'Ms.', 'incidunt', 0, 1, 1);
+INSERT INTO `AirPort` (`ID`, `Name`, `AirPortCode`, `PriorityOrder`, `LocationID`, `AirportOperatorID`) VALUES (2, 'Dr.', 'autem', 29026644, 2, 2);
+INSERT INTO `AirPort` (`ID`, `Name`, `AirPortCode`, `PriorityOrder`, `LocationID`, `AirportOperatorID`) VALUES (3, 'Mr.', 'id', 2559223, 3, 3);
+INSERT INTO `AirPort` (`ID`, `Name`, `AirPortCode`, `PriorityOrder`, `LocationID`, `AirportOperatorID`) VALUES (4, 'Mrs.', 'ducimus', 610, 4, 4);
+INSERT INTO `AirPort` (`ID`, `Name`, `AirPortCode`, `PriorityOrder`, `LocationID`, `AirportOperatorID`) VALUES (5, 'Dr.', 'consequatur', 44003716, 5, 5);
+INSERT INTO `AirPort` (`ID`, `Name`, `AirPortCode`, `PriorityOrder`, `LocationID`, `AirportOperatorID`) VALUES (6, 'Mrs.', 'optio', 373507, 6, 6);
+INSERT INTO `AirPort` (`ID`, `Name`, `AirPortCode`, `PriorityOrder`, `LocationID`, `AirportOperatorID`) VALUES (7, 'Prof.', 'qui', 0, 7, 7);
+INSERT INTO `AirPort` (`ID`, `Name`, `AirPortCode`, `PriorityOrder`, `LocationID`, `AirportOperatorID`) VALUES (8, 'Mr.', 'incidunt', 7505, 8, 8);
+INSERT INTO `AirPort` (`ID`, `Name`, `AirPortCode`, `PriorityOrder`, `LocationID`, `AirportOperatorID`) VALUES (9, 'Miss', 'quo', 26081, 9, 9);
+INSERT INTO `AirPort` (`ID`, `Name`, `AirPortCode`, `PriorityOrder`, `LocationID`, `AirportOperatorID`) VALUES (10, 'Prof.', 'fugit', 14189, 10, 10);
